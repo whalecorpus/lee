bf98c4c41a0692c9ae15012f8ddc3514bd736691
whalecorpus <annawalsh221@gmail.com>
2022-02-05 16:16:14 -0500

    2.0 switch about contact order, make font smaller, separate out work formatting

--- setup.js
+++ setup.js
@@ -1,5 +1,4 @@
-var items = ["lightblue", "#66d68b", "#66d6c3", "#79d666", "#8b66d6", "#d68b66", "#66b1d6", "#d666b1",
-"#b6bbd6", "#d6b6bb", "#b6cbd6", "#d0b6d6"];
+var items = ["lightblue", "#66d6c3", "#8b66d6", "#d68b66", "#66b1d6", "#d666b1", "#b6bbd6", "#d6b6bb", "#b6cbd6", "#d0b6d6"];
 var r = document.querySelector(':root');
 var rs = getComputedStyle(r);
 
